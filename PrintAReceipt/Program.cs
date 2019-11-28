5898d1ffeccc19818f4373e1a51a94c18f321a8d
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-11-28 18:58:40 +0000

    finish

---
+++ PrintAReceipt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAReceipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine().Split(' ').Select(decimal.Parse).ToArray();
+
+            Console.WriteLine("/{0}\\", new string('-', 22));
+            decimal total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("| {0,20:f2} |", numbers[i]);
+                total += numbers[i];
+            }
+
+            Console.WriteLine("|{0}|", new string('-', 22));
+            Console.WriteLine("| Total: {0,13:f2} |", 1000);
+            Console.WriteLine("\\{0}/", new string('-', 22));
+
+        }
+    }
+}
