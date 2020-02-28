621dfac3129e2d627067716a968d442352af9c03
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2020-02-28 15:17:36 +0000

    legendary farmin

--- LegendaryFarming/Program.cs
+++ LegendaryFarming/Program.cs
@@ -10,12 +10,10 @@ namespace LegendaryFarming
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string line = Console.ReadLine().ToLower();
+            string tokens 
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
         }
     }
 }
