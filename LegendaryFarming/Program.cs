0af8353f2f20d18568e87d45ee9cbfc391da44e8
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2020-03-05 17:58:14 +0000

    materials

--- LegendaryFarming/Program.cs
+++ LegendaryFarming/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LegendaryFarming
 {
@@ -10,10 +8,23 @@ namespace LegendaryFarming
     {
         static void Main(string[] args)
         {
-            string line = Console.ReadLine().ToLower();
-            string tokens 
+            string[] lineArgs = Console.ReadLine().ToLower().Split().ToArray();
 
+            var keyMaterialNames = new Dictionary<string, string>();
+            keyMaterialNames.Add("shards", "Shadownourne");
+            keyMaterialNames.Add("fragments", "Valanyr");
+            keyMaterialNames.Add("motes", "Dragonwrath");
 
+            var junkMaterials = new SortedDictionary<string, int>();
+            var keyMaterials = new Dictionary<string, int>();
+            keyMaterials.Add("shards", 0);
+            keyMaterials.Add("fragments", 0);
+            keyMaterials.Add("motes", 0);
+
+            for (int i = 0; i < lineArgs.Length - 2; i += 2)
+            {
+                if(junkMaterials.Contains()
+            }
         }
     }
 }
